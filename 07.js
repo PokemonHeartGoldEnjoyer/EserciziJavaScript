999d0abbe12edf1ef5c7263f0044e769887b506f
Endri Simakou <a.simakou@donboscosandona.it>
2021-11-29 13:15:54 +0100

    nooooooooooooooooooooooo

--- 07.js
+++ 07.js
@@ -1,1 +1,13 @@
-// tg
+// scrivere un programma che dati
+// il prezzo del biglietto
+let biglietto = 16;
+
+// e il numero di persone
+let persone = 6;
+
+// calcoli il totale da pagate
+let calcolo;
+calcolo = biglietto * persone;
+frase = " Il totale da pagare è di " + calcolo + "€.";
+// e lo scriva in una frase
+console.log(frase);
---
+++ 08.js
@@ -0,0 +1,40 @@
+// scrivere un programma che dati
+// il prezzo del biglietto
+// l'età delle persone
+// e il numero di persone
+// calcoli il totale da pagate
+// e lo scriva in una frase
+
+// nota: bisognerà definire due prezzi, uno per i minorenni
+// e uno per i maggiorenni
+
+
+// definisco il prezzo del biglietto intero
+let prezzoMaggiorenni = 8.50;
+
+// definisco il prezzo del biglietto intero
+let prezzoMinorenni = 5.45;
+
+// definisco il numero di persone
+let persone = 6;
+
+// definisco l'età
+let anni = 12;
+
+// definisco una variabile di totale
+let tot;
+for(tot = ) {
+    // se il sesso è maschile
+    if(anni >= 12) {
+    //      preparo una frase
+        frase = "Il prezzo da pagare è di " + tot ;
+    }
+    // altrimenti
+    else {
+    //      preparo l'altra
+        frase = "Benvenuta " + nome;
+    }
+
+    // scrivo la frase  
+    console.log(frase);
+}
