31ad9c489b3151ed9a8c24ab3e0bb1981c2f1373
Endri Simakou <a.simakou@donboscosandona.it>
2021-12-06 11:59:22 +0100

    13

---
+++ 14.js
@@ -0,0 +1,18 @@
+// definisco un array con nomidi pietre preziose
+
+let prfeziose = ['Rubino', 'Giada', 'Diamante'];
+
+// posso aggiungere un elemnto all'array con il metodo push:
+
+prfeziose.push('Smeraldo');
+
+// vediamol cosa c'è nell'array:
+console.log(preziose);
+
+
+// posso aggiungere un elemento anche all'inizio dell'array:
+
+preziose.unshift('Zaffiro');
+
+// vediamo cosa c'è nell'array:
+console.log(preziose);
