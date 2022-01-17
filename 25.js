7003c24118f394c79e8ff6abbbfc61f60ce6e003
Simakou Endri <e.simakou@donboscosandona.it>
2022-01-17 13:10:57 +0100

    4

---
+++ 25.js
@@ -0,0 +1,27 @@
+/*
+Dato il nome di un prodotto
+e un array contenente dei sottoarray del tipo [quantità, prodotto],
+incrementare di un’unità la quantità del prodotto corrispondente.
+*/
+
+let prodottoCercato = 'budino';
+
+let carrello = [
+['budino', 3],
+['detersivo', 1],
+['sturacessi', 1]
+];
+
+// SOLUZIONE
+// faccio un ciclo for che si passa tutti i prodotti del carrello
+for(let i=0; i<=carrello.length; i++) {
+// estraggo il nomero indicato da i
+    let nomero = carrello[i];
+// se il nome del prodotto è uguale (==) a quello cercato
+    if(nomero[0]==prodottoCercato) {
+    // aumento il contatore di quel prodotto di 1
+    nomero[1]++;
+    }
+}
+
+console.log(carrello)
