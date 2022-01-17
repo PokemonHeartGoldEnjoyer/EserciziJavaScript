3256a1981184e8e5e05edfae9a2d565824949464
Simakou Endri <e.simakou@donboscosandona.it>
2022-01-17 12:26:12 +0100

    y

---
+++ 23.js
@@ -0,0 +1,19 @@
+/*
+Dato un array contenente una serie di sottoarray del tipo ['nome',numero],
+fare in modo che il programma ripeta i vari nomi prt il numeri di volte indicato dal numnero.
+Ad esempio [['Paolo',2]['Marzia',3]] produrrà:
+Paolo
+Paolo
+Marzia
+Marzia
+Marzia
+*/
+
+// definisco l'array nomeri (nomi e numeri)
+    nomeri = [['Edoardo',6]['Martina',9]];
+
+// ciclo for che si passa tutti i nomeri
+for(let i=0; i<nomeri.length; i++) {
+// metto in una variabile il nomero corrente
+    let nommeri
+}
