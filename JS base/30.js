0fb551f40b797c521431c1cc6566352e9b6abb31
Simakou Endri <e.simakou@donboscosandona.it>
2022-01-24 12:55:14 +0100

    ihb

---
+++ JS base/30.js
@@ -0,0 +1,15 @@
+// posso passare come argomenti alla funzione e farle restituire anche delle stringhe
+
+// scrivere una funzione che se chiamata così:
+// nomeCompleto(nome,cognome)
+// restituisca una stringa così:
+// "sig. Luigi Pazzerelli"
+
+function nomeCompleto(nome,cognome) {
+    let = nomeCompleto = "sig." + nome + cognome
+    return nomeCompleto
+}
+
+let stringa = nomeCompleto("Luigi", " Pazzerelli")
+
+console.log(nomeCompleto)
