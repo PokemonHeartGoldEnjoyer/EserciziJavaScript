041a9925c8e3a9154c1476dfb29ef91730aa567f
Simakou Endri <e.simakou@donboscosandona.it>
2022-01-31 13:13:46 +0100

    hcm

---
+++ JS base/34.js
@@ -0,0 +1,29 @@
+// Definisci gli oggetti che servono per creare un'applicazione che faccia l'inventyario informatico del CFP.
+
+
+let Lab20 = {
+    tipo: 'aula',
+    oggetti: 17,
+    utilizzo: 'informatica',
+}
+
+let PcDidonna = {
+    tipo: 'computer',
+    componenti: 7,
+    utilizzatore: 'Didonna',
+}
+
+let Processore = {
+    tipo: 'Intel',
+    velocitaclock: 2.90 + "Ghz",
+    core: 6,
+    processorilogici: 6,
+}
+
+let Inventario = {
+    Aula: Lab20,
+    Computer: PcDidonna,
+    Pezzo: Processore,
+}
+
+console.log(Inventario)
