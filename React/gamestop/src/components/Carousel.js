7ac192fade4ada52994b5cd62344d30bcc918fa4
Simakou Endri <e.simakou@donboscosandona.it>
2022-02-21 11:47:48 +0100

    fhj

--- React/gamestop/src/components/Carousel.js
+++ React/gamestop/src/components/Carousel.js
@@ -1,6 +1,6 @@
 export default function Carousel(props) {
     let elementi = props.slides.map(slide =>
-        <div class="carousel-item">
+        <div class={slide.classi}>
                 <img src={ slide.immagine } class="d-block w-100" alt="..." />
             <div class="carousel-caption d-none d-md-block">
                 <h5>{ slide.titolo }</h5>
@@ -11,15 +11,9 @@ export default function Carousel(props) {
     return(
         <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
-                <div class="carousel-item active">
                 {elementi}
-                </div>
-                <div class="carousel-item">
                 {elementi}
-                </div>
-                <div class="carousel-item">
                 {elementi}
-                </div>
             </div>
             </div>
     )
